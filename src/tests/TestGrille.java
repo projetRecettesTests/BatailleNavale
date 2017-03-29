4f8bd1b30bcdd49eb49518086fce2e3cb8d0554e
Fanny-R <fanny.rottee@gmail.com>
2017-03-29 17:39:17 +0200

    Improve Grille Tests

--- src/tests/TestGrille.java
+++ src/tests/TestGrille.java
@@ -20,14 +20,31 @@ public class TestGrille
 	public void caseDansLaGrille()
 	{
 		assertNotNull(grille.accessCase(5, 1));
-		//assertEquals(8, grille.getDim1());
+	}
+	
+	@Test
+	public void caseDansLeCoinDeLaGrille()
+	{
+		assertNotNull(grille.accessCase(8, 2));
+	}
+	
+	
+	@Test
+	public void caseHorsLigne()
+	{
+		assertNull(grille.accessCase(15, 1));
+	}
+	
+	@Test
+	public void caseHorsColonne()
+	{
+		assertNull(grille.accessCase(7, 56));
 	}
 	
 	@Test
 	public void caseHorsLimites()
 	{
-		assertNull(grille.accessCase(10, 1));
+		assertNull(grille.accessCase(10, 5));
 	}
-
 
 }
