f01c1b837ca1aa01dda98347950ca6bec6fc1e3a
fanny.rottee@gmail.com <martin.bolot@etu.univ-lyon1.fr>
2017-03-29 16:41:22 +0200

    Add constructor in Grille

--- src/batailleNavale/Grille.java
+++ src/batailleNavale/Grille.java
@@ -2,5 +2,15 @@ package batailleNavale;
 
 public class Grille
 {
+	private Case[][] grille;
+	private int dim1;
+	private int dim2;
 
+	public Grille(int dim1, int dim2)
+	{
+		this.dim1 = dim1;
+		this.dim2 = dim2;
+		this.grille = new Case[dim1][dim2];
+	}
+	
 }
