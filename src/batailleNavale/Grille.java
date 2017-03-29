d381284662aafd4252d8e01bbc970ac6e564501f
fanny.rottee@gmail.com <martin.bolot@etu.univ-lyon1.fr>
2017-03-29 17:15:48 +0200

    Improve Grille

--- src/batailleNavale/Grille.java
+++ src/batailleNavale/Grille.java
@@ -11,6 +11,44 @@ public class Grille
 		this.dim1 = dim1;
 		this.dim2 = dim2;
 		this.grille = new Case[dim1][dim2];
+		fillGrille();
+
 	}
-	
+
+	private void fillGrille()
+	{
+		for (int i = 0; i < dim1; i++)
+		{
+			for (int j = 0; j < dim2; j++)
+			{
+				grille[i][j] = new Case(i, j);
+			}
+		}
+	}
+
+	public Case accessCase(int x, int y)
+	{
+		Case laCase = null;
+
+		try
+		{
+			laCase = grille[x][y];
+		} catch (ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("La case n'est pas dans la grille !");
+		}
+
+		return laCase;
+	}
+
+	public int getDim1()
+	{
+		return dim1;
+	}
+
+	public int getDim2()
+	{
+		return dim2;
+	}
+
 }
