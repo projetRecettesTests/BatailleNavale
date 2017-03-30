20c600f570f03f3adaef6184c45839bdfcdb9354
lucyhebert <lucyhebert@free.fr>
2017-03-30 13:37:16 +0200

    Improve testPlaceBateau

--- src/tests/testPlaceBateau.java
+++ src/tests/testPlaceBateau.java
@@ -5,15 +5,39 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import batailleNavale.FregateHorizontale;
+import batailleNavale.Grille;
+
 public class testPlaceBateau {
 
+	Grille grille;
+	FregateHorizontale bateau1;
+	FregateHorizontale bateau2;
+	FregateHorizontale bateau3;
+	
 	@Before
 	public void setUp() throws Exception {
+		grille = new Grille(10,20);
+		bateau1 = new FregateHorizontale();
+		bateau2 = new FregateHorizontale();
+		bateau3 = new FregateHorizontale();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPlaceBateauDispo() {
+		assertEquals(true, grille.placeBateau(bateau1,2,2));
+	}
+	
+	@Test
+	public void testPlaceBateauOccupied() {
+		assertEquals(true, grille.placeBateau(bateau1,2,2));
+		assertEquals(false, grille.placeBateau(bateau2,3,2));
+	}
+	
+	@Test
+	public void testPlaceBateauHorsGrille() {
+		assertEquals(true, grille.placeBateau(bateau1,2,2));
+		assertEquals(false, grille.placeBateau(bateau2,11,20));
 	}
 
 }
