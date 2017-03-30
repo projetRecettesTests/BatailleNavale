af3c3ab821096941053b47b3c0ef231c04309308
lucyhebert <lucyhebert@free.fr>
2017-03-30 11:51:59 +0200

    Implements placing boats

---
+++ src/batailleNavale/FregateHorizontale.java
@@ -0,0 +1,10 @@
+package batailleNavale;
+
+public class FregateHorizontale extends Bateau {
+	
+	public FregateHorizontale() {
+		super("Fregate Horizontale", 2, 1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
