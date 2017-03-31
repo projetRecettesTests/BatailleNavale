75279fd7f3f037dd2ae6daacaa8a34f3f978aff9
Fanny-R <fanny.rottee@gmail.com>
2017-03-31 14:42:56 +0200

    Create boats ugly

--- src/batailleNavale/Game.java
+++ src/batailleNavale/Game.java
@@ -28,12 +28,26 @@ public class Game
 		
 		do {
 			
-			System.out.println("Quel type de bateau souhaitez-vous placer ? 1 : Frégate");
+			System.out.println("Quel type de bateau souhaitez-vous placer ? 1 : Torpilleur Horizontal 2 : Torpilleur Vertical 3 : Croiseur Horizontal 4 : Croiseur Vertical 5 : Porte-Avion Horizontal 6 : Porte-Avion Vertical");
 			
 			switch(s.nextInt()) {
 			case 1 :
-				bateau = new FregateHorizontale();
+				bateau = new Bateau("Torpilleur Horizontal", 2, 1);
+			case 2 :
+				bateau = new Bateau("Torpilleur Vertical", 1, 2);
+			case 3 :
+				bateau = new Bateau("Croiseur Horizontal", 4, 1);
+			case 4 :
+				bateau = new Bateau("Croiseur Vertical", 1, 4);
+			case 5 :
+				bateau = new Bateau("Porte-Avion Horizontal", 5, 1);
+			case 6 :
+				bateau = new Bateau("Porte-Avion Vertical", 1, 5);
+			default :
+				System.out.println("Merci de saisir une valeur présente dans la liste");
 			}
+			
+			//To do : mettre ça dans une liste ? Vérifier que le numéro saisi est proposé dans la liste, sinon redemander
 			
 			System.out.println("Veuillez entrer les coordonnées du coin inférieur gauche du bateau : ");
 			System.out.print("x : ");
